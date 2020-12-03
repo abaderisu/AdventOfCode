1778f47af6474bac5c140223e2f633135ee2f119
Adam Bader <abader@agleader.com>
2020-12-03 15:30:03 -0600

    Simplifying day 2

--- 2020/day2/part1.py
+++ 2020/day2/part1.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
 from parse import compile
 
 p = compile('{min:d}-{max:d} {char}: {password}')
 
 valid = 0
-with open(sys.argv[1]) as f:
-    for line in f.readlines():
-        parsed = p.parse(line)
-        c = parsed['password'].count(parsed['char'])
-        if c >= parsed['min'] and c <= parsed['max']:
-            valid+=1
-       
+for parsed in [p.parse(line.strip()) for line in open('input.txt')]:
+    c = parsed['password'].count(parsed['char'])
+    if c >= parsed['min'] and c <= parsed['max']:
+        valid+=1
+   
 print(valid) 
--- 2020/day2/part2.py
+++ 2020/day2/part2.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 
-import sys
 from parse import compile
 
 p = compile('{first:d}-{second:d} {char}: {password}')
 
-valid = 0
-with open(sys.argv[1]) as f:
-    for line in f.readlines():
-        parsed = p.parse(line)
-        password = parsed['password']
-        first = password[parsed['first'] - 1] == parsed['char']
-        second = password[parsed['second'] - 1] == parsed['char']
-        if first ^ second:
-            valid+=1
-       
+lines = valid = 0
+for parsed in [p.parse(line.strip()) for line in open('input.txt')]:
+    password = parsed['password']
+    first = password[parsed['first'] - 1] == parsed['char']
+    second = password[parsed['second'] - 1] == parsed['char']
+    if first ^ second:
+        valid+=1
+   
 print(valid) 
