d41a442713e98bf72b131d4cc7dc5a13ddade9c6
Adam Bader <abader@agleader.com>
2020-12-05 01:54:47 -0600

    comprehension day 5

--- 2020/day5/part1.py
+++ 2020/day5/part1.py
@@ -5,21 +5,7 @@ import re
 lines = [line.strip() for line in open('input.txt')]
 ids = []
 for line in lines:
-    rmin = 0
-    rmax = 127
-    for i in range(0,7):
-        half = int(64 / (1 << i))
-        if line[i] == 'F':
-            rmax -= half
-        else:
-            rmin += half
-    cmin = 0
-    cmax = 7
-    for i in range(0,3):
-        half = int(4 / (1 << i))
-        if line[i + 7] == 'L':
-            cmax -= half
-        else:
-            cmin += half
-    ids.append(rmin * 8 + cmin)
+    row = sum([int(64 / (1<<i)) for i in range(0,7) if line[i] == 'B'])
+    col = sum([int(4 / (1 << i)) for i in range(0, 3) if line[i + 7] == 'R'])
+    ids.append(row * 8 + col)
 print(max(ids))
