a0dcf322fd6a395f54783627c392870eb229c851
Adam Bader <abader@agleader.com>
2020-12-05 01:28:26 -0600

    day 5

---
+++ 2020/day5/part2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import re
+
+lines = [line.strip() for line in open('input.txt')]
+ids = []
+for line in lines:
+    rmin = 0
+    rmax = 127
+    for i in range(0,7):
+        half = int(64 / (1 << i))
+        if line[i] == 'F':
+            rmax -= half
+        else:
+            rmin += half
+    cmin = 0
+    cmax = 7
+    for i in range(0,3):
+        half = int(4 / (1 << i))
+        if line[i + 7] == 'L':
+            cmax -= half
+        else:
+            cmin += half
+    ids.append(rmin * 8 + cmin)
+
+ids.sort()
+for i in range(0,len(ids)):
+    if ids[i + 1] != ids[i] + 1:
+        print(ids[i] + 1)
+        break
