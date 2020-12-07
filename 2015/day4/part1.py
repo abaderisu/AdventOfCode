d0fd467edab6016c82b6d3f8df5a9cf844e51c69
Adam Bader <abader@agleader.com>
2020-12-06 21:38:38 -0600

    2015 day 4

---
+++ 2015/day4/part1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import hashlib
+
+key = 'ckczppom'
+dec = 0
+while True:
+    hash = hashlib.md5((key + str(dec)).encode('utf-8')).hexdigest()
+    if ( hash.startswith('00000') ):
+        break
+    dec += 1
+print(dec)
